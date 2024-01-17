e60b257164dd013f538d6a6926f63e32e374a84e
painisreal <143738001+painisreal@users.noreply.github.com>
2024-01-17 19:37:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// !nav bar toggle menu switch
+var sidebar = document.getElementById("sidebar"); // ?side-menu-bar
+var closebtn = document.getElementById("close"); // ?"X"btn
+
+function shownav() {
+  sidebar.style.left = "0%";
+}
+function closenav() {
+  sidebar.style.left = "-60%";
+}
+
+
